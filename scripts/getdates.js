f526be766c5344b2aa08ff66d81f37bbb4e6f721
stewarterin <erinlu@live.com>
2024-04-27 23:47:19 -0600

    01 Assignment

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+let ccYear = new Date().getFullYear();
+document.getElementById("year").innerHTML = `&copy; ${ccYear}`
+
+
+let modifiedDate = new Date(document.lastModified);
+document.getElementById("lastModified").innerHTML = `Last Modified: ${modifiedDate}`;
