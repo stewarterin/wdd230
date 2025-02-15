17d1e909b4c2ea2f0b5138c61f9eea8ab49826ea
stewarterin <erinlu@live.com>
2025-02-15 01:32:02 -0700

    javascript code fixed

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -1,5 +1,2 @@
-let ccYear = new Date().getFullYear();
-document.getElementById("year").innerHTML = `${ccYear}`
-
 let modifiedDate = new Date(document.lastModified);
 document.getElementById("lastModified").innerHTML = `Last Modified: ${modifiedDate}`;
