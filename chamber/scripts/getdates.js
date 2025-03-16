7264e4ef584637dad7ba0df4cd100c911e8599f4
stewarterin <erinlu@live.com>
2025-03-15 23:15:35 -0600

    join page

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -1,2 +1,4 @@
 let modifiedDate = new Date(document.lastModified);
 document.getElementById("lastModified").innerHTML = `Last Modified: ${modifiedDate}`;
+
+document.getElementById("timeload").value = Date.now();
