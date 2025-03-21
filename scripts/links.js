ce03b69c586c07eaeec636bfc3d70e69f0a6b3dd
stewarterin <erinlu@live.com>
2025-03-20 23:13:02 -0600

    json links added to learning activities section

--- scripts/links.js
+++ scripts/links.js
@@ -4,7 +4,31 @@ const linksURL = "https://stewarterin.github.io/wdd230/data/links.json";
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
+    console.table(data.lessons);
+    displayLinks(data.lessons);
 }
 
 getLinks();
+
+const displayLinks = (lessons) => {
+    lessons.forEach((lesson) => {
+        let list = document.createElement('li');
+        let title = document.createElement('h4');
+
+        title.textContent = `${lesson.lesson}: `;
+        list.appendChild(title);
+
+        lesson.links.forEach((link) => {
+            let linkEntry = document.createElement('li');
+            let anchor = document.createElement('a');
+
+            anchor.href = link.url;
+            anchor.textContent = link.title;
+
+            linkEntry.appendChild(anchor);
+            list.appendChild(linkEntry);
+        });
+
+        document.getElementById('links').appendChild(list);
+    });
+}
