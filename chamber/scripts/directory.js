c70f82ecac84036c795a0441632306b1cb590230
stewarterin <erinlu@live.com>
2025-03-22 18:43:03 -0600

    directory finished

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -2,6 +2,10 @@ const baseURL = "https://stewarterin.github.io/wdd230/";
 const linksURL = "https://stewarterin.github.io/wdd230/chamber/data/members.json";
 const cards = document.querySelector('#cards');
 
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
 async function getMemberData() {
     const response = await fetch(linksURL);
     const data = await response.json();
@@ -17,10 +21,13 @@ const displayMembers = (members) => {
         let address = document.createElement('p');
         let phone = document.createElement('p');
         let logo = document.createElement('img');
+        let website = document.createElement('a');
 
-        name.textContent = `${member.name}`;
-        address.textContent = `${member.address}`;
-        phone.textContent = `${member.phone}`;
+        name.textContent = member.name;
+        address.textContent = member.address;
+        phone.textContent = member.phone;
+        website.textContent = member.website;
+        website.href = member.website;
 
         logo.setAttribute('src', member.icon);
         logo.setAttribute('alt', `Logo for ${member.name}`);
@@ -31,8 +38,21 @@ const displayMembers = (members) => {
         card.appendChild(name);
         card.appendChild(address);
         card.appendChild(phone);
+        card.appendChild(website);
         card.appendChild(logo);
 
         cards.appendChild(card);
     });
+}
+
+gridbutton.addEventListener("click", () => {
+    display.classList.add("grid");
+    display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList);
+
+function showList() {
+    display.classList.add("list");
+    display.classList.remove("grid");
 }
