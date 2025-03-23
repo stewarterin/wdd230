6cf478fb0672faa5c121212106e6256dd721f218
stewarterin <erinlu@live.com>
2025-03-22 22:16:36 -0600

    trying more fixes

--- scripts/responsivemenu.js
+++ scripts/responsivemenu.js
@@ -1,7 +1,9 @@
-const mainnav = document.querySelector('.navigation')
-const hambutton = document.querySelector('#menu');
+const mainnav = document.querySelector(".navigation");
+const hambutton = document.querySelector("#menu");
 
 hambutton.addEventListener('click', () => {
     mainnav.classList.toggle('show');
     hambutton.classList.toggle('show');
 });
+
+document.getElementById("joinButton").onclick = () => window.location.href = "join.html";
